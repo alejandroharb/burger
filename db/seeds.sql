bb0649848d04cc91f7325893f3d96b39aa76102c
alejandroharb <alejandroharb@gmail.com>
2017-02-14 11:39:30 -0600

    created schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("cheese burger", true), ("quarter pounder", false), ("Bean Patty burger", false), ("Quinoa burger", true), ("Angus Beef burger", true);
