08c334c33ef8acead3859dfb1ef74ffdaf48e341
alejandroharb <alejandroharb@gmail.com>
2017-02-15 09:05:09 -0600

    created crontrollers public and views directories

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+//import burger models
+var burger = require('../models/burger.js');
+
+//exports routes for server.js
+module.exports = router;
